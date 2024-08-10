70f9f7e00f5fc869ca737a4eef88a14b909e7c0f
jainArchit1 <architsupper@gmail.com>
2024-08-10 06:01:09 +0530

    first commit

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { SignUp } from "@clerk/nextjs";
+const SignupPage = () => {
+  return <SignUp></SignUp>;
+};
+
+export default SignupPage;
---
+++ app/(root)/creadits/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreaditPage = () => {
+  return <div>creadits</div>;
+};
+
+export default CreaditPage;
